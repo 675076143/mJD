7374ac7a64c5f04dd320815af5baca9f4e20527f
RobinJohn <robin675076143@hotmail.com>
2019-11-07 09:09:36 +0800

    图片高度自适应

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,30 @@
 import css from '../css/index.less';
 import html from '../index.html'
+
+
+
+/*
+加载页面时执行
+ */
+window.onload = ()=>{
+    selfAdaption()
+}
+window.onresize = ()=>{
+    selfAdaption()
+}
+
+/*
+自适应
+ */
+const selfAdaption =()=>{
+    const elementTopNavImgs = document.getElementsByClassName("top-nav-img")
+    //将伪数组转成真数组
+    const topNavImgs = [...elementTopNavImgs]
+    //top-nav中的图片自适应缩放
+    topNavImgs.forEach((topNavImg)=>{
+        topNavImg.style.height = topNavImg.clientWidth+"px"
+    })
+
+
+
+}
